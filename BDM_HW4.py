8cfabbc7ef515fdf55d6370f1d9dcf82fc293cc4
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-20 12:28:55 -0400

    First commit, testing the files

---
+++ BDM_HW4.py
@@ -0,0 +1,24 @@
+import sys
+import pyspark
+from pyspark import SparkContext
+
+def sample_func(shapefile):
+
+	import fiona.crs
+    import geopandas as gpd
+
+
+    zones = gpd.read_file(shapefile).to_crs(fiona.crs.from_epsg(2263))
+    print(zones.head())
+
+
+
+if __name__=='__main__':
+    sc = SparkContext()
+    input_file = sys.argv[1]
+    taxi = sc.textFile(input_file)
+    print(taxi.head())
+    sample_func('hdfs:///tmp/bdm/neighborhoods.geojson')
+    sample_func('hdfs:///tmp/bdm/boroughs.geojson')
+
+
