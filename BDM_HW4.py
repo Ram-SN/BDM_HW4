0630cf25f186c239b18afc086bace533914d0d0e
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-20 16:21:46 -0400

    First commit, testing the files

--- BDM_HW4.py
+++ BDM_HW4.py
@@ -3,16 +3,15 @@ import pyspark
 from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
 from pyspark.sql import SQLContext
-import geopandas as gpd
 
 
 
-# def sample_func(shapefile):
-#     import fiona.crs
-#     import geopandas as gpd
+def sample_func(shapefile):
+    import fiona.crs
+    import geopandas as gpd
 
-#     zones = gpd.GeoDataFrame(shapefile)
-#     print(zones.take(10))
+    zones = gpd.GeoDataFrame(shapefile)
+    print(zones.take(10))
 
 
 
@@ -20,16 +19,17 @@ if __name__=='__main__':
     sc = SparkContext()
     sqlContext = SQLContext(sc)
     spark = SparkSession(sc)
+
     input_file = sys.argv[1]
+
     taxi = sc.textFile(input_file)
     print(taxi.take(10))
-    neighborhoods = 'hdfs:///tmp/bdm/neighborhoods.geojson'
-    zones = gpd.read_file(neighborhoods).to_crs(fiona.crs.from_epsg(2263))
-    print(zones.head())
+
+    # neighborhoods = sqlContext.read.json('hdfs:///tmp/bdm/neighborhoods.geojson')
     #print(neighborhoods.printSchema())
     # neighborhoods = sc.textFile('hdfs:///tmp/bdm/neighborhoods.geojson')
     # boroughs = sc.textFile('hdfs:///tmp/bdm/boroughs.geojson')
-    # sample_func(neighborhoods)
-    # # sample_func(boroughs)
+    sample_func('neighborhoods.geojson')
+    # sample_func(boroughs)
 
 
