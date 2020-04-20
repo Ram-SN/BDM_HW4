31d3f711ca77279ed4d645b7d46c0175ecc3b73c
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-20 13:03:14 -0400

    First commit, testing the files

--- BDM_HW4.py
+++ BDM_HW4.py
@@ -6,7 +6,7 @@ def sample_func(shapefile):
     import fiona.crs
     import geopandas as gpd
 
-    zones = gpd.read_file(shapefile).to_crs(fiona.crs.from_epsg(2263))
+    zones = gpd.read_file(shapefile)
     print(zones.take(10))
 
 
@@ -16,7 +16,7 @@ if __name__=='__main__':
     input_file = sys.argv[1]
     taxi = sc.textFile(input_file)
     print(taxi.take(10))
-    sample_func('hdfs:/tmp/bdm/neighborhoods.geojson')
-    sample_func('hdfs:/tmp/bdm/boroughs.geojson')
+    sample_func('hdfs:///tmp/bdm/neighborhoods.geojson')
+    sample_func('hdfs:///tmp/bdm/boroughs.geojson')
 
 
