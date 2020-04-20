336a8881a2aa07ec8a586a3a293d9c4cff1659b9
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-20 13:19:12 -0400

    First commit, testing the files

--- BDM_HW4.py
+++ BDM_HW4.py
@@ -16,7 +16,9 @@ if __name__=='__main__':
     input_file = sys.argv[1]
     taxi = sc.textFile(input_file)
     print(taxi.take(10))
-    sample_func('/tmp/bdm/neighborhoods.geojson')
-    sample_func('/tmp/bdm/boroughs.geojson')
+    neighborhoods = sc.textFile('hdfs:///tmp/bdm/neighborhoods.geojson')
+    boroughs = sc.textFile('hdfs:///tmp/bdm/boroughs.geojson')
+    sample_func(neighborhoods)
+    sample_func(boroughs)
 
 
