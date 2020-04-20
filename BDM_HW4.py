36a4342b1da01a5c723e00c379a532f0a3e606a0
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-20 12:36:23 -0400

    First commit, testing the files

--- BDM_HW4.py
+++ BDM_HW4.py
@@ -7,7 +7,7 @@ def sample_func(shapefile):
     import geopandas as gpd
 
     zones = gpd.read_file(shapefile).to_crs(fiona.crs.from_epsg(2263))
-    print(zones.head())
+    print(zones.take(10))
 
 
 
@@ -15,7 +15,7 @@ if __name__=='__main__':
     sc = SparkContext()
     input_file = sys.argv[1]
     taxi = sc.textFile(input_file)
-    print(taxi.head())
+    print(taxi.take(10))
     sample_func('hdfs:///tmp/bdm/neighborhoods.geojson')
     sample_func('hdfs:///tmp/bdm/boroughs.geojson')
 
