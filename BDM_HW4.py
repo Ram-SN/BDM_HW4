7da759a748ec92907725198521b67955b90b87ba
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-21 13:40:45 -0400

    Testing the file outputs

--- BDM_HW4.py
+++ BDM_HW4.py
@@ -6,7 +6,7 @@ from pyspark.sql import SQLContext
 
 
 
-def sample_func():
+def sample_func(pid, records):
     import fiona.crs
     import geopandas as gpd
     
@@ -27,6 +27,6 @@ if __name__=='__main__':
     
     #sample_func()
 
-    counts = mapPartitionsWithIndex(sample_func) \
+    counts = taxi.mapPartitionsWithIndex(sample_func) \
         .collect()
     
