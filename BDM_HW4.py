def0d50944e589ffbfa8c3737daf88fe935ef2fa
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-21 12:35:00 -0400

    Testing the file outputs

--- BDM_HW4.py
+++ BDM_HW4.py
@@ -9,19 +9,16 @@ from pyspark.sql import SQLContext
 def sample_func():
     import fiona.crs
     import geopandas as gpd
-    
-    # read_json_file = sqlContext.read.json(in_file)
+
     in_file = 'neighborhoods.geojson'
     zones = gpd.read_file(in_file).to_crs(fiona.crs.from_epsg(2263))
     print(zones.head())
-    # print(read_json_file.printSchema())
-    # zones = gpd.read_file(shapefile)
-    # print(zones.take(10))
+    in_file = 'boroughs.geojson'
+    zones = gpd.read_file(in_file).to_crs(fiona.crs.from_epsg(2263))
+    print(zones.head())
 
 
 if __name__=='__main__':
-    import fiona.crs
-    import geopandas as gpd
     sc = SparkContext()
     sqlContext = SQLContext(sc)
     spark = SparkSession(sc)
@@ -29,17 +26,7 @@ if __name__=='__main__':
     input_file = sys.argv[1]
 
     taxi = sc.textFile(input_file)
-    # print(list(enumerate(taxi.first().split(','))))
-
-    # neighborhoods = sqlContext.read.json('hdfs:///tmp/bdm/neighborhoods.geojson')
-    # print(neighborhoods.printSchema())
-    # boroughs = sqlContext.read.json('hdfs:///tmp/bdm/boroughs.geojson')
-
-
-    # neighborhoods = sc.textFile('hdfs:///tmp/bdm/neighborhoods.geojson')
     
     sample_func()
-    # sample_func('hdfs:///tmp/bdm/boroughs.geojson')
-    # sample_func(boroughs)
-
+    
 
