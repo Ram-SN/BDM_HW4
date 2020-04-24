5e8c48a74d716a252a167c73ab2d37099c7869cd
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-24 18:48:28 -0400

    Changes made in PYNB and pushed as script

--- BDM_HW4.py
+++ BDM_HW4.py
@@ -4,19 +4,66 @@ from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
 from pyspark.sql import SQLContext
 
-
-
-def sample_func(pid, records):
+def createIndex(shapefile):
+    '''
+    This function takes in a shapefile path, and return:
+    (1) index: an R-Tree based on the geometry data in the file
+    (2) zones: the original data of the shapefile
+    
+    Note that the ID used in the R-tree 'index' is the same as
+    the order of the object in zones.
+    '''
+    import rtree
     import fiona.crs
     import geopandas as gpd
-    
-    in_file = 'neighborhoods.geojson'
-    zones = gpd.read_file(in_file).to_crs(fiona.crs.from_epsg(2263))
-    return(zones.head())
+    zones = gpd.read_file(shapefile).to_crs(fiona.crs.from_epsg(2263))
+    index = rtree.Rtree()
+    for idx,geometry in enumerate(zones.geometry):
+        index.insert(idx, geometry.bounds)
+    return (index, zones)
 
-    # in_file = 'boroughs.geojson'
-    # zones = gpd.read_file(in_file).to_crs(fiona.crs.from_epsg(2263))
-    # print(zones.head())
+def findZone(p, index, zones):
+    '''
+    findZone returned the ID of the shape (stored in 'zones' with
+    'index') that contains the given point 'p'. If there's no match,
+    None will be returned.
+    '''
+    match = index.intersection((p.x, p.y, p.x, p.y))
+    for idx in match:
+        if zones.geometry[idx].contains(p):
+            return idx
+    return None
+
+def processTrips(pid, records):
+    '''
+    Our aggregation function that iterates through records in each
+    partition, checking whether we could find a zone that contain
+    the pickup location.
+    '''
+    import csv
+    import pyproj
+    import shapely.geometry as geom
+    
+    # Create an R-tree index
+    proj = pyproj.Proj(init="epsg:2263", preserve_units=True)    
+    index, zones = createIndex('neighborhoods.geojson')    
+    
+    # Skip the header
+    if pid==0:
+        next(records)
+    reader = csv.reader(records)
+    counts = {}
+    
+    for row in reader:
+
+        p = geom.Point(proj(float(row[10]), float(row[11])))
+        
+        # Look up a matching zone, and update the count accordly if
+        # such a match is found
+        zone = findZone(p, index, zones)
+        if zone:
+            counts[zone] = counts.get(zone, 0) + 1
+    return counts.items()
 
 
 if __name__=='__main__':
@@ -24,11 +71,14 @@ if __name__=='__main__':
     input_file = sys.argv[1]
 
     taxi = sc.textFile(input_file)
-    
-    #sample_func()
 
-    counts = taxi.mapPartitionsWithIndex(sample_func) \
-        .collect()
+    counts = taxi.mapPartitionsWithIndex(processTrips) \
+            .reduceByKey(lambda x,y: x+y) \
+            .collect()
 
-    print(counts)
+    countsPerNeighborhood = list(map(lambda x: (zones['neighborhood'][x[0]],zones['borough'][x[0]], x[1]), counts))
+    df = pd.DataFrame(countsPerNeighborhood)
     
+    testing=df.sort_values([1,2], ascending=[True,False]).groupby(1).head(5)
+
+    print((testing))
