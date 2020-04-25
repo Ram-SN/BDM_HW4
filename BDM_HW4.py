60df39ed5c20b32beb409d8c1f6b678650449e2d
Ram Sowmya Narayanan <rangamannar98@gmail.com>
2020-04-24 21:33:07 -0400

    Changes made in PYNB and pushed as script

--- BDM_HW4.py
+++ BDM_HW4.py
@@ -57,7 +57,7 @@ def processTrips(pid, records):
     
     for row in reader:
 
-        p = geom.Point(proj(float(row[10]), float(row[11])))
+        p = geom.Point(proj(float(row[9]), float(row[10])))
         
         # Look up a matching zone, and update the count accordly if
         # such a match is found
